e5963f524e93da4c65984d8eae51281110b53b4b
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-10 16:42:04 -0700

    update database to include identity

--- AnimalShelterApi/Migrations/20220610233850_add identity to database.Designer.cs
+++ AnimalShelterApi/Migrations/20220610233850_add identity to database.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using AnimalShelterApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AnimalShelterApi.Migrations
 {
     [DbContext(typeof(AnimalShelterApiContext))]
-    partial class AnimalShelterApiContextModelSnapshot : ModelSnapshot
+    [Migration("20220610233850_add identity to database")]
+    partial class addidentitytodatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
