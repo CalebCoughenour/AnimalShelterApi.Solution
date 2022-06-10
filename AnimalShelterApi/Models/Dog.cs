ead63c48a7fbf4ab1f092f90e1b43c6f5cf465f7
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-10 11:43:50 -0700

    change animal to dog model

---
+++ AnimalShelterApi/Models/Dog.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalShelterApi.Models
+{
+  public class Dog
+  {
+    public int DogId { get; set; }
+    [Required]
+    [StringLength(20, ErrorMessage = "Name must be 20 or less characters")]
+    public string Name { get; set; }
+    [Required]
+    [StringLength(50, ErrorMessage = "Species must be 50 or less characters")]
+    public string Species { get; set; }
+    [Range(1, 200)]
+    public int Age { get; set; }
+    public string Gender { get; set; }
+    [Required]
+    public DateTime DateCreated  { get; set; }
+    public Dog()
+    {
+      DateCreated = DateTime.Now;
+    }
+
+  }
+}
