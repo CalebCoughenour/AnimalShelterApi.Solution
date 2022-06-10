459e1b835a75ffe403ffb6bb1879d56096510e22
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-10 14:57:13 -0700

    add string restrictions for gender

--- AnimalShelterApi/Models/Cat.cs
+++ AnimalShelterApi/Models/Cat.cs
@@ -12,8 +12,9 @@ namespace AnimalShelterApi.Models
     [Required]
     [StringLength(50, ErrorMessage = "Species must be 50 or less characters")]
     public string Species { get; set; }
-    [Range(1, 200)]
+    [Range(1, 30, ErrorMessage = "Age must be less than 30")]
     public int Age { get; set; }
+    [StringLength(20, ErrorMessage = "Gender type must be 20 or less characters")]
     public string Gender { get; set; }
     public DateTime DateCreated  { get; set; }
     public Cat()
