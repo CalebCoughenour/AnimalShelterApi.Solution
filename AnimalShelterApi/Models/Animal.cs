29be747d630ff6fb944285c0ab7829bd7ad09c31
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-10 09:41:30 -0700

    add properties for creating an animal with date/time of creation included

---
+++ AnimalShelterApi/Models/Animal.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalShelterApi
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    [Required]
+    [StringLength(20, ErrorMessage = "Name must be 20 or less characters")]
+    public string Name { get; set; }
+    [Required]
+    [StringLength(50, ErrorMessage = "Species must be 50 or less characters")]
+    public string Species { get; set; }
+    [Range(1, 200)]
+    public string Age { get; set; }
+    public string Gender { get; set; }
+    public DateTime DateCreated  { get; set; }
+    public Animal()
+    {
+      DateCreated = DateTime.Now;
+    }
+
+  }
+}
