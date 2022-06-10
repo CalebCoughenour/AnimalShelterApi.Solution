98f93891723868b34cd2bdad2dc7ebe7e289ebb5
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-10 16:30:43 -0700

    add DTOs for user registration

---
+++ AnimalShelterApi/Models/DTOs/Requests/UserRegistrationDTO.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalShelterApi.Models.DTOs.Requests
+{
+  public class UserRegistrationDto
+  {
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    public string Password { get; set; }
+  }
+}
---
+++ AnimalShelterApi/Models/DTOs/Responses/RegistrationResponse.cs
@@ -0,0 +1,9 @@
+using AnimalShelterApi.Configuration;
+
+namespace AnimalShelterApi.Models.DTOs.Responses
+{
+   public class RegistrationResponse : AuthResult
+  {
+    
+  }
+}
